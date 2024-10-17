7cda1d3e83296969357f9bb2b4c446a531d209ba
Lauriane <lauriane.marquesmartins@gmail.com>
2024-10-17 10:32:55 +0200

    Etape 3

--- 01_bonjour_javascript2/script.js
+++ 01_bonjour_javascript2/script.js
@@ -1,9 +1,11 @@
+function sayHello(firstName){
 let message = "Bonjour !"
-
-console.log(message)
-
-let firstName = "Beyoncé"
 
 message = `Bonjour ${firstName} !`
 
 console.log(message)
+}
+
+let firstName = "Beyoncé"
+
+sayHello(firstName)
