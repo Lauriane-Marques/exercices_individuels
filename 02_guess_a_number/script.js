075e978356bf4a8177743a85bac8674418798e1c
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2024-10-24 14:40:19 +0200

    Etape 2

--- 02_guess_a_number/script.js
+++ 02_guess_a_number/script.js
@@ -1,6 +1,29 @@
 function userNumber (){
-    let givenNumber=  prompt("Entrez un chiffre")
-    return givenNumber
+    let userChoice =  Number(prompt("Entrez un chiffre")) //transformation de la chaine de caractère en variable number
+    return userChoice
  }
+
+let givenNumber = userNumber()
  
- console.log(userNumber())
+console.log(givenNumber)
+
+function didIWin(userGuess){
+    userGuess = userNumber()
+    if (userGuess<22){
+        alert("Plus grand")
+    }
+    else if (userGuess>22) {
+        alert("Plus petit")
+    }
+    else {
+        alert("Bravo! Vous avez deviné le nombre.")
+    }
+ }
+
+
+const gamePlay = () => {
+    userNumber()
+    didIWin(givenNumber)
+ }
+
+ // Cette partie fonctionne mais le prompt s'affiche 3 fois avant de me donner ma réponse (mais la réponse est correcte)
