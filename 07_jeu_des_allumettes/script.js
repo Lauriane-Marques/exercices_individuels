7fdc8288e40a6692c15d1539465887d32f7091a0
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2024-11-14 12:29:27 +0100

    start step 3 + basic css

--- 07_jeu_des_allumettes/script.js
+++ 07_jeu_des_allumettes/script.js
@@ -1,18 +1,49 @@
-let matchesInit = 50
+const playerInput = document.querySelector('#player1')
+const takeOutBtn = document.querySelector('#validBtn')
+const matchesLeft = document.querySelector('#matchesLeft')
 
-function askPlayer() {
-    let playerChoice = Number(prompt("Combien d'allumettes voulez-vous retirer ?"))
-    return playerChoice
-}
+let totalMatches = 50
+
+//display the number of matches at the beginning of the game
+matchesLeft.innerHTML = `Matches left : <br/> ${totalMatches}`
 
 
+//function taking out the number of matches selected by the player
 function takeMatches(matchesNumber){
-    matchesInit = matchesInit - matchesNumber
-    return matchesInit
+  totalMatches = totalMatches - matchesNumber
+  return totalMatches
 }
 
-while (matchesInit > 0){
-    let playerMatches = askPlayer()
-    takeMatches(playerMatches)
-    console.log(matchesInit)
+//validation of the number written in the input
+function validation() {
+    const playerChoice = Number(playerInput.value)
+    if (playerChoice >= 1 && playerChoice <= 6) { //limits the number selected by the player between 1 and 6 included
+        playerInput.value ='' //clears the input
+        takeMatches(playerChoice)
+        console.log(playerChoice);
+        console.log(totalMatches)
+        matchesLeft.innerHTML = `Matches left : <br/> ${totalMatches}`
+        if (totalMatches < 0){
+          console.log("Le jeu est fini ")
+        }
+        return playerChoice
+    } else {
+      alert("Please choose a number between 1 and 6")
+    }
+  
 }
+
+//enables validation through the Enter key
+playerInput.addEventListener('keydown', function(evenement) {
+    if (evenement.key === 'Enter') {
+      validation();
+    }
+  });
+
+//confirms the player's choice through the button
+takeOutBtn.addEventListener('click', validation)
+
+
+
+//Change player name
+//take matches out
