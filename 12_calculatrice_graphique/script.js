f7fcb56a28cb48472f4af0f37204a56ceebbb7f8
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-01-31 14:03:54 +0100

    step 2

--- 12_calculatrice_graphique/script.js
+++ 12_calculatrice_graphique/script.js
@@ -3,16 +3,31 @@ const NUM_CONTAINER = document.getElementById('numbers')
 let screen = document.getElementById('screen')
 
 
-for (let i = 1; i <=9;i++){
-    button = document.createElement('button')
-    NUM_CONTAINER.appendChild(button)
-    button.innerText = i
-    button.id = `btn${i}`
-    button.after(document.getElementById('btn0'))
-    button.after(document.getElementById('btn-plus'))
-}
+// for (let i = 1; i <=9;i++){
+//     button = document.createElement('button')
+//     NUM_CONTAINER.appendChild(button)
+//     button.innerText = i
+//     button.id = `btn${i}`
+//     button.after(document.getElementById('btn0'))
+//     button.after(document.getElementById('btn-plus'))
+// }
 
 // const btn1 = document.getElementById('btn1');
 // btn1.addEventListener('click', () => {
 //   console.log('1');
 // });
+
+function displayCalc (pointer) {
+
+    const clickedNumber = pointer.target; 
+    const number = clickedNumber.textContent;
+    console.log(number)
+
+    screen.innerText += number
+}
+
+let btns = document.querySelectorAll('button');
+btns.forEach( btn => {
+
+    btn.addEventListener('click', displayCalc);
+})
