649bbcad40b0a7ea593b277648b2ff8109d523b6
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-02-04 16:14:28 +0100

    step 2

--- 16_superglobales_php/exercice.php
+++ 16_superglobales_php/exercice.php
@@ -1,18 +1,26 @@
 <html>
 <body>
 
-<h1>Bonjour 
+<form action="exercice.php" method="post">
+ <p>Votre nom : <input type="text" name="input" /></p>
+ <p><input type="submit" value="OK" name="submit-button"></p>
+</form>
+
     <?php 
 
-$firstname = $_GET["first_name"]; 
+$Gfirstname = $_GET["first_name"];
+$Pfirstname = $_POST["input"]; 
 
-    if ($firstname) {
-        echo $firstname;
-    } else{
-        echo 'anonyme';
+    if (isset($_POST["input"])) {
+        $Pfirstname = $_POST["input"];
+        echo "Hello $Pfirstname via POST";
+    } elseif ($Gfirstname){
+        echo "Hello $Gfirstname via GET";
+    }else{
+        echo 'Hello anonyme';
     }
-    
     ?>
+
 
 </body>
 </html>
