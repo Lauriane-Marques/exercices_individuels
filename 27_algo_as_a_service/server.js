13a1fbe52d3643fb5098fd3af29a803ccc85e32b
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-03-28 10:06:22 +0100

    step 1

--- 27_algo_as_a_service/server.js
+++ 27_algo_as_a_service/server.js
@@ -4,6 +4,17 @@ const port = 3000
 
 app.use(express.json())
 
+app.post("/sort", (req, res) => {
+    const order = req.body.sort_order
+    const arr = req.body.values
+
+    if (order == 'asc'){
+        res.send(`Order the following values : [${arr}] by ascending order`)
+        } else {
+        res.send(`Order the following values : [${arr}] by descending order`)
+    }
+})
+
 app.listen(port, (error) =>{
     if(!error)
         console.log("Server is running on port "+ port)
